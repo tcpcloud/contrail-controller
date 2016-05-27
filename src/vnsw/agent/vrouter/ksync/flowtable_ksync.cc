83a3df1f9301fe04d69e901fcf6fddfd5fc99f69
Naveen N <naveenn@juniper.net>
2016-05-27 15:02:52 +0530

    * Agent changes to support QoS
    1> Qos Queue, forwarding-class and qos-config oper DB tables are added
    2> qos-config support on interface, virtual-network , vhost and fabric
    interface
    3> Flow action can also specify qos-config as action
    Test case for same
    Partial-bug:#1571493
    
    Change-Id: Ib3a9918ba881e10145ed820452998ec1b1ea7afa

--- src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
@@ -133,6 +133,7 @@ void FlowTableKSyncEntry::Reset() {
     last_event_ = FlowEvent::INVALID;
     token_.reset();
     ksync_response_info_.Reset();
+    qos_config_idx = AgentQosConfigTable::kInvalidIndex;
 }
 
 void FlowTableKSyncEntry::Reset(FlowEntry *flow, uint32_t hash_id) {
@@ -365,6 +366,7 @@ int FlowTableKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
         req.set_fr_flags(flags);
         req.set_fr_action(action);
         req.set_fr_drop_reason(drop_reason);
+        req.set_fr_qos_id(qos_config_idx);
     }
 
     FlowProto *proto = ksync_obj_->ksync()->agent()->pkt()->get_flow_proto();
@@ -454,6 +456,11 @@ bool FlowTableKSyncEntry::Sync() {
     }
     if (src_nh_id_ != nh_id) {
         src_nh_id_ = nh_id;
+        changed = true;
+    }
+
+    if (qos_config_idx != flow_entry_->data().qos_config_idx) {
+        qos_config_idx = flow_entry_->data().qos_config_idx;
         changed = true;
     }
 
