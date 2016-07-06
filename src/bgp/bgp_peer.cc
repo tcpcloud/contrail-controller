1a112105e416b8bec65410e7cb72d4027aaaca3e
Ananth Suryanarayana <anantha@juniper.net>
2016-07-06 13:02:03 -0700

    Add bgp [llgr-]graceful-restart times (in seconds) to introspect and to uve
    This is sent from the peer via [LL-]GR capability
    
    Change-Id: Ieeaa19ec5d54d732cf6ebe893ab90f9edd92305d
    Partial-Bug: #1537933

--- src/bgp/bgp_peer.cc
+++ src/bgp/bgp_peer.cc
@@ -1641,6 +1641,9 @@ bool BgpPeer::SetGRCapabilities(BgpPeerInfoData *peer_info) {
             &long_lived_graceful_restart_families_);
     peer_info->set_long_lived_graceful_restart_families(
             long_lived_graceful_restart_families_);
+    peer_info->set_graceful_restart_time(peer_close_->GetGracefulRestartTime());
+    peer_info->set_long_lived_graceful_restart_time(
+            peer_close_->GetLongLivedGracefulRestartTime());
     BGPPeerInfoSend(*peer_info);
 
     // If GR is no longer supported, terminate GR right away. This can happen
@@ -2377,6 +2380,10 @@ void BgpPeer::FillNeighborInfo(const BgpSandeshContext *bsc,
     bnr->set_graceful_restart_address_families(graceful_restart_families_);
     bnr->set_long_lived_graceful_restart_address_families(
             long_lived_graceful_restart_families_);
+    bnr->set_graceful_restart_time(peer_close_->GetGracefulRestartTime());
+    bnr->set_long_lived_graceful_restart_time(
+            peer_close_->GetLongLivedGracefulRestartTime());
+
     bnr->set_configured_hold_time(state_machine_->GetConfiguredHoldTime());
     FillBgpNeighborFamilyAttributes(bnr);
     FillBgpNeighborDebugState(bnr, peer_stats_.get());
